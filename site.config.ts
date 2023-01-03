0e35f9b95521f5ce22bb09036fb2436baa7a93e9
koalaonbed <34034076+koalaonbed@users.noreply.github.com>
2023-01-03 21:50:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'db28d01595ad4212bbe541b511a3a016',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Chen-Chin Lin Portfolio',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Chen-Chin Lin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
